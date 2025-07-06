d7f278c434054163959a8b7270c483a6812c6f8f
Jesse <Jessymunene9@gmail.com>
2025-07-06 22:08:53 +0300

    additional tables

--- task_2.sql
+++ task_2.sql
@@ -1,44 +1,45 @@
--- CREATE DATABASE IF NOT EXISTS
+-- CREATE DATABASE
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS AUTHORS (
+-- Table: Authors
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- CREATE TABLE: BOOKS
-CREATE TABLE IF NOT EXISTS BOOKS (
+-- Table: Books
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- CREATE TABLE: CUSTOMERS
-CREATE TABLE IF NOT EXISTS CUSTOMERS (
+-- Table: Customers
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
 );
 
--- CREATE TABLE: ORDERS
-CREATE TABLE IF NOT EXISTS ORDERS (
+-- Table: Orders
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- CREATE TABLE: ORDER_DETAILS
-CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
+-- Table: Order Details
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
